3ec0bdac3c8e02ccb7ee6c41b6a7fbaf8f203e63
Jiayuli-CU <jiayu.li@columbia.edu>
2022-03-14 19:14:07 +0800

    change db from mysql to postgresql

--- Project/build.sbt
+++ Project/build.sbt
@@ -3,6 +3,10 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "3.1.1"
 
 lazy val root = (project in file("."))
-  .settings(
-    name := "Project"
-  )
+    .settings(
+        name := "Project",
+        libraryDependencies ++= Seq(
+            "mysql" % "mysql-connector-java" % "8.0.28",
+            "io.getquill" % "quill-jdbc_2.13" % "3.16.3"
+        )
+    )
--- Project/src/main/recourses/database setup/insert.sql
+++ Project/src/main/recourses/database setup/insert.sql
@@ -1,21 +1,20 @@
-insert into Course(`course_id`,
-                   `course_name`,
-                   `institution`,
-                   `course_url`)
+insert into "Course"(course_id,
+                     course_name,
+                     institution,
+                     course_url)
 values ('machine-learning',
         'Machine Learning',
         'Stanford University',
         'https://www.coursera.org/learn/machine-learning');
 
-insert into Reviewer(`reviewer_name`)
+insert into "Reviewer"(reviewer_name)
 values ('Li Jiayu');
 
-insert into Review(`review_date`,
-                   `rating`,
-                   `reviewer_id`,
-                   `course_id`)
+insert into "Review"(review_date,
+                     rating,
+                     reviewer_id,
+                     course_id)
 values ('2022-02-02',
         5,
         1,
         'machine-learning');
-
